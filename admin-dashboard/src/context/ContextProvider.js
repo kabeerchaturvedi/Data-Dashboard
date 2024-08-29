f66dc81da2f72e8a27bf5720bb86441ba9ef6c97
Kabeer Chaturvedi <kbrchaturvedi@gmail.com>
2024-08-29 18:17:56 +0530

    Navbar Completion

--- admin-dashboard/src/context/ContextProvider.js
+++ admin-dashboard/src/context/ContextProvider.js
@@ -3,17 +3,26 @@ import React, { act, Children, createContext, useContext, useState } from "react
 const StateContext = createContext();
 
 const initialState = {
-  chat: false, 
+  chat: false,
   cart: false,
   userProfile: false,
   notification: false,
 };
 
-
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined)
 
-  return <StateContext.Provider value={{ activeMenu,setActiveMenu }}>{children}</StateContext.Provider>;
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+
+  return (
+    <StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked,handleClick,screenSize,setScreenSize }}>
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
